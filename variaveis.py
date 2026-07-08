aa4b518eea514307bda21a0fe91ed3af98b178bf
ph-gusmao <fox_max_power@outlook.com>
2026-07-08 00:00:35 -0300

    feat: finalizando variaveis

--- variaveis.py
+++ variaveis.py
@@ -1,2 +1,27 @@
-nome = "Pedro"  # string
-print(nome)
+# nome_usuario = "Pedro"  # string
+# print(nome_usuario)
+
+# media = 0
+# n1 = n2 = n3 = n4 = 0.0
+# nome, idade = "Pedro", 27
+# estado = True
+
+# Função type()
+# print(type(media))
+# print(type(n2))
+# print(type(nome))
+# print(type(estado))
+# print(type(1 + 2j))
+# ctrl + k + c comenta
+
+# Função isinstance()
+# Compara o tipo de uma variável com um tipo escolhido por voce
+# Comparou a variável 'a' com o tipo 'int'
+# a = 10
+# b = "Sol"
+# print(isinstance(a, int))
+
+a = 40
+c = 3
+r = a * c
+print(r)
