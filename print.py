f77e83d67891a0aa544484f16059367159a045ed
ph-gusmao <fox_max_power@outlook.com>
2026-07-10 23:58:48 -0300

    feat: formatação de textos e contatenação

---
+++ print.py
@@ -0,0 +1,34 @@
+# Sintaxe:
+# print(objetos(textos, por exemplo), argumentos)
+
+# nome = "Pedro Gusmão"
+# print("Treinamento Pyhton -", nome)
+# estudos = "Treinamento Python"
+# print(estudos, "-", nome)
+
+# nome = input("Digite seu nome: ")
+# msg = "Olá " + nome + ", seja bem-vindo(a) ao treinamento Python!"
+# print(msg)
+
+# print("Imprime a mensagem e muda de linha automaticamente")
+# print("Imprime a mensagem e permanece na linha.", end=" ")
+# print(" Continuo na mesma linha!")
+
+# nome = "Maria"
+# idade = 30
+# msg_formatada = "O nome dela é {0} e ela tem {1} anos.".format(nome, idade)
+# print(msg_formatada)
+
+# nome = "Pedro"
+# peso = 76.50
+# msg = f"Olá, meu nome é {nome} e meu peso é {peso} kg."
+# print(msg)
+
+# a = 10
+# b = 5
+
+# res = f"A soma de {a} com {b} é igual a {a + b}"
+# print(res)
+
+valor = 125.579637
+print(f"O valor é {valor:.3f}")
