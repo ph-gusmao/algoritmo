0ee2cddee79f41d2e01f8a0c5b41bc8f0b6b46b5
ph-gusmao <fox_max_power@outlook.com>
2026-07-10 23:08:35 -0300

    feat: operadores logicos

---
+++ operadores-logicos.py
@@ -0,0 +1,21 @@
+# idade = 25
+# altura = 1.75
+
+# resultado = idade >= 18 and altura >= 1.70
+
+# msg = "Pode participar do evento?" + str(resultado)
+
+# print(msg)
+
+# Programa de disparo de alarme
+# porta = "a"
+# janela = "a"
+
+# alarme = porta == "a" or janela == "a"
+# print("Alarme disparado?" + str(alarme))
+
+tem_dinheiro = False
+tem_dinheiro = not tem_dinheiro
+
+msg = "Tem dinheiro? " + str(tem_dinheiro)
+print(msg)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+idade = 25
+altura = 1.75
+
+resultado = idade >= 18 and altura >= 1.70
+
+msg = "Pode participar do evento?" + str(resultado)
+
+print(msg)
