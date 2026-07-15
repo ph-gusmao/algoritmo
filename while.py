8ca747980da384fb5cb7acba54086e3f036e2893
ph-gusmao <fox_max_power@outlook.com>
2026-07-14 21:40:55 -0300

    feat: while finish

--- while.py
+++ while.py
@@ -1,6 +1,17 @@
-num = 1
+# num = 1
 
-while num <= 10:
-    print(num)
-    num = num + 1
-    # ou num += 1
+# while num <= 100:
+#     print(num)
+#     num = num + 1
+#     # ou num += 1
+
+nome = None
+
+while True:
+    nome = input("Digite seu nome, ou X para sair: ")
+
+    if nome == "x" or nome == "X":
+        break
+    else:
+        print(f"Bem vindo, {nome}")
+print("Obrigado!")
